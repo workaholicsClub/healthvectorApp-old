bce322cbba8847abc7c19d2d63328459b9bf2149
Anna-Sentyakova <anna.sent@gmail.com>
2017-02-25 14:02:06 +0400

    state strategy type

--- app/src/main/java/ru/android/childdiary/presentation/splash/SplashView.java
+++ app/src/main/java/ru/android/childdiary/presentation/splash/SplashView.java
@@ -2,13 +2,13 @@ package ru.android.childdiary.presentation.splash;
 
 import android.support.annotation.Nullable;
 
-import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import ru.android.childdiary.data.types.Sex;
 import ru.android.childdiary.presentation.core.BaseActivityView;
 
-@StateStrategyType(OneExecutionStateStrategy.class)
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface SplashView extends BaseActivityView {
     void startApp(@Nullable Sex sex);
 }
