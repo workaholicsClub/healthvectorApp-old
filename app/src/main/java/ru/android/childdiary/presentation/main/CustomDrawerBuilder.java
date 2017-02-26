b590ec3a7d3e9b0fe205e2b310058ebfa09de18c
Anna-Sentyakova <anna.sent@gmail.com>
2017-02-26 13:35:11 +0400

    visibility

--- app/src/main/java/ru/android/childdiary/presentation/main/CustomDrawerBuilder.java
+++ app/src/main/java/ru/android/childdiary/presentation/main/CustomDrawerBuilder.java
@@ -2,7 +2,7 @@ package ru.android.childdiary.presentation.main;
 
 import com.mikepenz.materialdrawer.DrawerBuilder;
 
-public class CustomDrawerBuilder extends DrawerBuilder {
+class CustomDrawerBuilder extends DrawerBuilder {
     public void closeDrawerWithoutAnimation() {
         if (mDrawerLayout != null) {
             mDrawerLayout.closeDrawer(mDrawerGravity, false);
