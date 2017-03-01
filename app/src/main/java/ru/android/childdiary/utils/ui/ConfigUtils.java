e2ce1eee280ec9c172a37d582676a4a5fcc48c13
Anna <anna.sent@gmail.com>
2017-03-01 14:40:49 +0400

    orientation fix

--- app/src/main/java/ru/android/childdiary/utils/ui/ConfigUtils.java
+++ app/src/main/java/ru/android/childdiary/utils/ui/ConfigUtils.java
@@ -3,12 +3,8 @@ package ru.android.childdiary.utils.ui;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 
-import ru.android.childdiary.BuildConfig;
-
 public class ConfigUtils {
     public static void setupOrientation(Activity activity) {
-        if (!BuildConfig.DEBUG) {
-            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
