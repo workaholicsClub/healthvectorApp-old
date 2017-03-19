27fde9359c6dfd62a65ef3abe9589193096564f6
Anna-Sentyakova <anna.sent@gmail.com>
2017-03-19 16:16:12 +0400

    not swipeable view pager

---
+++ app/src/main/java/ru/android/childdiary/presentation/main/widgets/NotSwipeableViewPager.java
@@ -0,0 +1,26 @@
+package ru.android.childdiary.presentation.main.widgets;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NotSwipeableViewPager extends ViewPager {
+    public NotSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NotSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
