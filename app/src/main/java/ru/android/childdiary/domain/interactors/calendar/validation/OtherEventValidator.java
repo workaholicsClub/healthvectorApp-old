0b8034b7179abacdbed1fe151f3cc2d5b8111f40
Anna-Sentyakova <anna.sent@gmail.com>
2017-03-25 07:53:58 +0400

    CEV-131: Поле Наименование события Другое -- обязательное

--- app/src/main/java/ru/android/childdiary/domain/interactors/calendar/validation/OtherEventValidator.java
+++ app/src/main/java/ru/android/childdiary/domain/interactors/calendar/validation/OtherEventValidator.java
@@ -2,14 +2,15 @@ package ru.android.childdiary.domain.interactors.calendar.validation;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import ru.android.childdiary.R;
 import ru.android.childdiary.domain.core.Validator;
-import ru.android.childdiary.domain.interactors.calendar.events.standard.FeedEvent;
 import ru.android.childdiary.domain.interactors.calendar.events.standard.OtherEvent;
 
 public class OtherEventValidator extends Validator<OtherEvent, CalendarValidationResult> {
@@ -23,6 +24,13 @@ public class OtherEventValidator extends Validator<OtherEvent, CalendarValidatio
     @Override
     public List<CalendarValidationResult> validate(@NonNull OtherEvent event) {
         List<CalendarValidationResult> results = new ArrayList<>();
+
+        if (TextUtils.isEmpty(event.getTitle())) {
+            CalendarValidationResult result = new CalendarValidationResult();
+            result.addMessage(context.getString(R.string.validate_event_other_title_empty));
+            results.add(result);
+        }
+
         return results;
     }
 }
