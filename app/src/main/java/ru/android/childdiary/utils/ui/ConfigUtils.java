4dd24e0697fce80c7c9752cb2b7f9ed6fac142be
Anna-Sentyakova <anna.sent@gmail.com>
2017-04-05 09:49:37 +0400

    CEV-187: Обновить иконку для события Другое на экране детальной инф. по событию

--- app/src/main/java/ru/android/childdiary/utils/ui/ConfigUtils.java
+++ app/src/main/java/ru/android/childdiary/utils/ui/ConfigUtils.java
@@ -1,9 +1,10 @@
 package ru.android.childdiary.utils.ui;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 
 public class ConfigUtils {
     public static void setupOrientation(Activity activity) {
-        //activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
