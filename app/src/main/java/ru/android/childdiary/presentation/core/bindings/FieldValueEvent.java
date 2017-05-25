9b524edcc2c8d658d4cee7d5d46dccbe13ceea0b
Anna-Sentyakova <anna.sent@gmail.com>
2017-05-26 02:22:23 +0400

    proguard enabled

--- app/src/main/java/ru/android/childdiary/presentation/core/bindings/FieldValueEvent.java
+++ app/src/main/java/ru/android/childdiary/presentation/core/bindings/FieldValueEvent.java
@@ -2,11 +2,13 @@ package ru.android.childdiary.presentation.core.bindings;
 
 import android.support.annotation.Nullable;
 
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
 import ru.android.childdiary.presentation.core.fields.widgets.FieldValueView;
 
 @Value
+@AllArgsConstructor(suppressConstructorProperties = true)
 public class FieldValueEvent<T> {
     @NonNull
     FieldValueView<T> view;
--- app/src/main/java/ru/android/childdiary/presentation/core/bindings/SearchViewQueryTextEvent.java
+++ app/src/main/java/ru/android/childdiary/presentation/core/bindings/SearchViewQueryTextEvent.java
@@ -2,10 +2,12 @@ package ru.android.childdiary.presentation.core.bindings;
 
 import android.support.v7.widget.SearchView;
 
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
 
 @Value
+@AllArgsConstructor(suppressConstructorProperties = true)
 public class SearchViewQueryTextEvent {
     @NonNull
     SearchView view;
